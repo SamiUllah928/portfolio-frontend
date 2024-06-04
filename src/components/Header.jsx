7fef9b92d946ed1938dda863080961236f242bdf
Sami Ullah <samiullahiqbal5@gmail.com>
2024-06-05 03:25:16 +0500

    commited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiBars3BottomRight } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 // import { useState } from "useState";
 export default function Header() {
-  // const [home, setHome] = useState();
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
-  const handleClicked = () => {};
+  const handleClicked = () => {
+    setOpen(!open);
+  };
   return (
-    <div className="header">
-      <div className="left">
-        <h1>Port</h1>
-        <h1>Folio.</h1>
+    <div className="header-main">
+      <div className="header">
+        <div className="left">
+          <h1>Port</h1>
+          <h1>Folio.</h1>
+        </div>
+        <HiBars3BottomRight className="bar" onClick={handleClicked} />
+
+        <div className="right ">
+          <ul>
+            <li onClick={() => navigate("/")}>
+              <a>Home</a>
+            </li>
+            <li onClick={() => navigate("/services")}>
+              <a>Services</a>
+            </li>
+            <li onClick={() => navigate("/skills")}>
+              <a>Skills</a>
+            </li>
+            <li onClick={() => navigate("/project")}>
+              <a>Projects</a>
+            </li>
+            <li onClick={() => navigate("/about")}>
+              <a>About</a>
+            </li>
+            <li onClick={() => navigate("/contact")}>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <HiBars3BottomRight className="bar" onClick={handleClicked} />
-      <div className="right">
-        <ul>
-          <li onClick={() => navigate("/")}>
-            <a></a>
-          </li>
-          <li onClick={() => navigate("/services")}>
-            <a>Services</a>
-          </li>
-          <li onClick={() => navigate("/skills")}>
-            <a>Skills</a>
-          </li>
-          <li onClick={() => navigate("/project")}>
-            <a>Projects</a>
-          </li>
-          <li onClick={() => navigate("/about")}>
-            <a>About</a>
-          </li>
-          <li onClick={() => navigate("/contact")}>
-            <a>Contact</a>
-          </li>
-        </ul>
-      </div>
+
+      {open && (
+        <div className="mobile-ul">
+          <ul>
+            <li onClick={() => navigate("/")}>
+              <a>Home</a>
+            </li>
+            <li onClick={() => navigate("/services")}>
+              <a>Services</a>
+            </li>
+            <li onClick={() => navigate("/skills")}>
+              <a>Skills</a>
+            </li>
+            <li onClick={() => navigate("/project")}>
+              <a>Projects</a>
+            </li>
+            <li onClick={() => navigate("/about")}>
+              <a>About</a>
+            </li>
+            <li onClick={() => navigate("/contact")}>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
